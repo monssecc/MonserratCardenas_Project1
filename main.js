46fdc0a4b983a9e0a7b0efeef818175e0fd202ae
Monserrat Cárdenas <156544483+monssecc@users.noreply.github.com>
2025-10-11 14:59:58 -0400

    Added the two buttons book and shorts

--- main.js
+++ main.js
@@ -23,3 +23,64 @@ import { createListings } from './listings.js';
 // createNavigation(pokemonTypes)
 // createListings(pokemonTypes)
 
+const booksButton = document.getElementById('books');
+const shortsButton = document.getElementById('shorts');
+
+booksButton.addEventListener('click', async () => {
+  const response = await fetch('https://stephen-king-api.onrender.com/api/books');
+  const json = await response.json();
+  console.log('Books', json.data);
+  renderBooks(json.data);
+});
+shortsButton.addEventListener('click', async () => {
+  const response = await fetch('https://stephen-king-api.onrender.com/api/shorts');
+  const json = await response.json();
+  console.log('Short', json.data);
+  //createListings(json.items);
+});
+
+
+const renderBooks = (books) => {
+  const bookList = document.getElementById('bookList');
+  bookList.innerHTML = '';
+  books.forEach(book => {
+    const div = document.createElement('div');
+    div.classList.add('bookItem');
+
+    // e.g. 25 from https://pokeapi.co/api/v2/pokemon/25/
+          const id = book.id
+ // assign a unique ID to the popover accounting for pokemon having multiple types
+          const popoverId = `${book.Title}-${id}`
+
+    // the template includes a button to open the popover
+    // as well as a placeholder for the popover itself.
+    const iconURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+    let template =
+      `<button class="open" popoverTarget="${popoverId}" >
+              <img src="${iconURL}" onError="this.src='pokeball.svg'"/>
+              <span>${book.Title}</span>
+              <span>${book.Year}</span>
+              <img class="open" src="open.svg" />
+            </button>
+            <div popover id="${popoverId}">
+              <div class="profile">
+                <p>Loading...</p>
+              </div>
+            </div>`
+    div.innerHTML = DOMPurify.sanitize(template)
+    // when the popover is opened, fetch details and build a profile
+    // we only do this when opened to avoid excessive API calls 
+    div.querySelector(`#${popoverId}`)
+      .addEventListener('toggle', async (event) => {
+        if (event.newState == 'open') {
+          //event.target.innerHTML = await createProfile(popoverId, item.pokemon.url)
+          console.log('book clicked');
+        }
+      })
+
+
+            
+  bookList.appendChild(bookItem);
+  });
+}
+
