da9b70386cd8a9203db729281d514a6a7fb004ae
Monserrat Cárdenas <156544483+monssecc@users.noreply.github.com>
2025-10-12 12:29:45 -0400

    Render short, and fetch url from shorts

--- main.js
+++ main.js
@@ -36,6 +36,7 @@ shortsButton.addEventListener('click', async () => {
   const response = await fetch('https://stephen-king-api.onrender.com/api/shorts');
   const json = await response.json();
   console.log('Short', json.data);
+  renderShorts(json.data);
   //createListings(json.items);
 });
 
@@ -54,13 +55,12 @@ const renderBooks = (books) => {
 
     // the template includes a button to open the popover
     // as well as a placeholder for the popover itself.
-    const iconURL = `https://cdn.vectorstock.com/i/500p/11/48/cartoon-stack-of-books-icon-vector-51211148.jpg`
+    //const iconURL = `https://cdn.vectorstock.com/i/500p/11/48/cartoon-stack-of-books-icon-vector-51211148.jpg`
+    //I also removed this <img src="${iconURL}" onError="this.src='pokeball.svg'"/>
     let template =
       `<button class="open" popoverTarget="${popoverId}" >
-              <img src="${iconURL}" onError="this.src='pokeball.svg'"/>
               <span>${book.Title}</span>
               <span>${book.Year}</span>
-              <img class="open" src="open.svg" />
             </button>
             <div popover id="${popoverId}">
               <div class="profile">
@@ -76,6 +76,52 @@ const renderBooks = (books) => {
         if (event.newState == 'open') {
           event.target.innerHTML = await createBookProfile(popoverId, bookurl)
           console.log('book clicked');
+        }
+      })
+
+
+            
+  bookList.appendChild(div);
+  });
+}
+
+
+//render shorts
+const renderShorts = (books) => {
+  const bookList = document.getElementById('bookList');
+  bookList.innerHTML = '';
+  books.forEach(book => {
+    const div = document.createElement('div');
+    div.classList.add('bookItem');
+
+    // e.g. 25 from https://pokeapi.co/api/v2/pokemon/25/
+          const id = book.id
+ // assign a unique ID to the popover accounting for pokemon having multiple types
+          const popoverId =  `book-${book.handle}-${id}`
+
+    // the template includes a button to open the popover
+    // as well as a placeholder for the popover itself.
+    //const iconURL = `https://cdn.vectorstock.com/i/500p/11/48/cartoon-stack-of-books-icon-vector-51211148.jpg`
+    //I also removed this <img src="${iconURL}" onError="this.src='pokeball.svg'"/>
+    let template =
+      `<button class="open" popoverTarget="${popoverId}" >
+              <span>${book.title}</span>
+              <span>${book.year}</span>
+            </button>
+            <div popover id="${popoverId}">
+              <div class="profile">
+                <p>Loading...</p>
+              </div>
+            </div>`
+    div.innerHTML = DOMPurify.sanitize(template)
+    const bookurl = `https://stephen-king-api.onrender.com/api/short/${book.id}`
+    // when the popover is opened, fetch details and build a profile
+    // we only do this when opened to avoid excessive API calls 
+    div.querySelector(`#${popoverId}`)
+      .addEventListener('toggle', async (event) => {
+        if (event.newState == 'open') {
+          event.target.innerHTML = await createShortProfile(popoverId, bookurl)
+          console.log('short clicked');
         }
       })
 
@@ -114,5 +160,34 @@ const createBookProfile = async (popoverId, url) => {
   </div>`
   return DOMPurify.sanitize(template)
 }
+
+// For shorts profile
+// this includes a template populated with details 
+// it also includes a close button.
+const createShortProfile = async (popoverId, url) => {
+  const data = await fetch(url)
+  const json = await data.json()
+  const book = json.data
+  //const imageURL = getProfileImageURL(book)
+  // build a template to hold details for this pokemon 
+  const template = ` 
+  <div class="profile">
+    <button class="close" popovertarget="${popoverId}"  >Close</button>
+    <div class="details"> 
+    
+      <div class="characteristics"> 
+        <h2>${book.title}</h2>
+        <h3>
+          ${ book.year}
+        </h3>
+
+      </div>
+      <div>${book.type}</div>
+
+    </div> 
+  </div>`
+  return DOMPurify.sanitize(template)
+}
+
 
 
